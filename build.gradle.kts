af7075f2a56288d416e6c7a87d92d9c8f2b0c2e6
Seyoung Cho <seyoungcho2@gmail.com>
2021-05-22 16:49:03 +0900

    [Example] 3. 프로젝트 수준의 build.gradle.kts 설정

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+task("clean", Delete::class) {
+    delete = setOf(rootProject.buildDir)
+}
